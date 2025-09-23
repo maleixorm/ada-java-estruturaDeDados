1e47831c691310164819f759bbe8b81757462ee0
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-23 16:30:52 -0300

    Criando o método print, para mostrar os itens da lista

--- src/main/java/dataStructure/LinkedList.java
+++ src/main/java/dataStructure/LinkedList.java
@@ -48,10 +48,19 @@ public class LinkedList {
         length = 0;
     }
 
+    public void print() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList("elemento 1");
         list.getHead();
         list.getTail();
         list.getLength();
+        list.print();
     }
 }
